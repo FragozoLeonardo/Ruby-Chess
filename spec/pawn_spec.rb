7ee708adaee0458134c5dbc9df17c348fba47c8f
Leonardo Fragozo <leo.fragozo.dev@gmail.com>
2023-08-16 17:31:52 -0300

    Add the pawn spec

--- spec/pawn_spec.rb
+++ spec/pawn_spec.rb
@@ -1,25 +1,50 @@
-# frozen_string_literal: true
-
-# spec/pawn_spec.rb
 require 'pawn'
+require 'validator'
 
 RSpec.describe Pawn do
-  let(:validator) { double('validator') }
+  let(:validator) { Validator.new }
+  let(:move) { Move.new(white_pawn) }
   let(:white_pawn) { Pawn.new('white', validator) }
   let(:black_pawn) { Pawn.new('black', validator) }
 
   describe '#valid_move?' do
     context 'when not capturing' do
-      it 'delegates to the validator' do
-        expect(validator).to receive(:valid_advance?).with('white', [1, 1], [2, 1], true)
-        white_pawn.valid_move?([1, 1], [2, 1])
+      it 'returns true for a valid advance move' do
+        start = [1, 1]
+        destination = [2, 1]
+        capture = false
+
+        expect(validator).to receive(:valid_advance?).with('white', start, destination, true).and_return(true)
+        expect(white_pawn.valid_move?(start, destination, capture)).to be true
+      end
+
+      it 'returns false for an invalid advance move' do
+        start = [1, 1]
+        destination = [3, 1]
+        capture = false
+
+        expect(validator).to receive(:valid_advance?).with('white', start, destination, true).and_return(false)
+        expect(white_pawn.valid_move?(start, destination, capture)).to be false
       end
     end
 
     context 'when capturing' do
-      it 'delegates to the validator' do
-        expect(validator).to receive(:valid_capture?).with('black', [6, 1], [5, 2])
-        black_pawn.valid_move?([6, 1], [5, 2], true)
+      it 'returns true for a valid capture move' do
+        start = [6, 1]
+        destination = [5, 2]
+        capture = true
+
+        expect(validator).to receive(:valid_capture?).with('black', start, destination).and_return(true)
+        expect(black_pawn.valid_move?(start, destination, capture)).to be true
+      end
+
+      it 'returns false for an invalid capture move' do
+        start = [6, 1]
+        destination = [5, 1]
+        capture = true
+
+        expect(validator).to receive(:valid_capture?).with('black', start, destination).and_return(false)
+        expect(black_pawn.valid_move?(start, destination, capture)).to be false
       end
     end
   end
